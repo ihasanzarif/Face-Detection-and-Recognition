e4f611cec57bb09e6bb4dae9bf36c496be3ae359
ihasanzarif <zarif3894@gmail.com>
2019-04-24 02:03:25 +0600

    face detection repository

---
+++ FaceDetectionAndRecognition/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace FaceDetectionAndRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cameraBox = new Emgu.CV.UI.ImageBox();
+            this.startBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.cameraBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cameraBox
+            // 
+            this.cameraBox.Location = new System.Drawing.Point(13, 13);
+            this.cameraBox.Name = "cameraBox";
+            this.cameraBox.Size = new System.Drawing.Size(536, 500);
+            this.cameraBox.TabIndex = 2;
+            this.cameraBox.TabStop = false;
+            // 
+            // startBtn
+            // 
+            this.startBtn.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startBtn.Location = new System.Drawing.Point(666, 49);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(264, 57);
+            this.startBtn.TabIndex = 3;
+            this.startBtn.Text = "Start Detection and Recognition";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 525);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.cameraBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.cameraBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox cameraBox;
+        private System.Windows.Forms.Button startBtn;
+    }
+}
+
---
+++ FaceDetectionAndRecognition/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace FaceDetectionAndRecognition
+{
+    public partial class Form1 : Form
+    {
+        HaarCascade faceDetected;
+        Image<Bgr, Byte> Frame;
+        Capture camera;
+        Image<Gray, Byte> result;
+        Image<Gray, Byte> trainedFace = null;
+        Image<Gray, Byte> grayFace = null;
+        List<Image<Gray, Byte>> trainingImages = new List<Image<Gray, byte>>();
+        List<string> labels = new List<string>();
+        List<string> users = new List<string>();
+        int count, numLabels, t;
+        string name, names = null;
+        public Form1()
+        {
+            InitializeComponent();
+            faceDetected = new HaarCascade("haarcascade_frontalface_default.xml");
+            try
+            {
+                string labelsInfo = File.ReadAllText(Application.StartupPath + "Faces/Faces.txt");
+                string[] Labels = labelsInfo.Split(',');
+                numLabels = Convert.ToInt16(Labels[0].Length);
+                count = numLabels;
+                string facesLoad;
+                for (int i = 1; i < numLabels + 1; i++)
+                {
+                    facesLoad = "face" + i + ".bmp";
+                    trainingImages.Add(new Image<Gray, byte>(Application.StartupPath+"/Faces/Faces.txt"));
+                    labels.Add(Labels[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(@"Nothing found.");
+            }
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            camera = new Capture();
+            camera.QueryFrame();
+            Application.Idle += new EventHandler(FrameProcedure);
+        }
+
+        private void FrameProcedure(object sender, EventArgs e)
+        {
+            users.Add("");
+            Frame = camera.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            grayFace = Frame.Convert<Gray, Byte>();
+            MCvAvgComp[][] facesDetectedNow = grayFace.DetectHaarCascade(faceDetected,1.2,10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+        }
+    }
+}
